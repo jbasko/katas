d9338828b1a91232693e2bdeeb42c4a828be6e45
jbasko <jazeps.basko@gmail.com>
2013-04-02 20:10:12 +0100

    Support Bonus - Leaf (arithmetic)

--- kata16.py
+++ kata16.py
@@ -6,25 +6,6 @@ from Queue import Queue
 from unittest.case import TestCase
 
 
-class Bonus(object):
-
-    def __init__(self, score=0, rolls=None):
-        self.score = score
-        self.rolls = rolls or []
-
-
-bonuses = [
-    Bonus(100, [1]),
-    Bonus(50, [5]),
-    Bonus(1000, [1, 1, 1]),
-    Bonus(200, [2, 2, 2]),
-    Bonus(300, [3, 3, 3]),
-    Bonus(400, [4, 4, 4]),
-    Bonus(500, [5, 5, 5]),
-    Bonus(600, [6, 6, 6]),
-]
-
-
 class Leaf(object):
 
     def __init__(self, score=0, rolls=None):
@@ -41,6 +22,34 @@ class Leaf(object):
                 return None
         return Leaf(score=self.score + bonus.score, rolls=rolls)
 
+    def __sub__(self, other):
+        if not isinstance(other, Leaf):
+            raise ValueError
+        rolls = list(self.rolls)
+        for i in range(len(other.rolls)):
+            try:
+                k = rolls.index(other.rolls[i])
+                del rolls[k]
+            except ValueError:
+                return None
+        return Leaf(score=self.score + other.score, rolls=rolls)
+
+
+class Bonus(Leaf):
+    pass
+
+
+bonuses = [
+    Bonus(100, [1]),
+    Bonus(50, [5]),
+    Bonus(1000, [1, 1, 1]),
+    Bonus(200, [2, 2, 2]),
+    Bonus(300, [3, 3, 3]),
+    Bonus(400, [4, 4, 4]),
+    Bonus(500, [5, 5, 5]),
+    Bonus(600, [6, 6, 6]),
+]
+
 
 def score(rolls):
 
@@ -55,7 +64,7 @@ def score(rolls):
         if best_leaf is None or best_leaf.score < current.score:
             best_leaf = current
         for b in bonuses:
-            leaf = current.get_bonus_leaf(b)
+            leaf = current - b
             if leaf:
                 fringe.put(leaf)
 
@@ -70,3 +79,10 @@ class GreedTest(TestCase):
         self.assertEqual(350, score([3, 4, 5, 3, 3]))
         self.assertEqual(250, score([1, 5, 1, 2, 4]))
         self.assertEqual(600, score([5, 5, 5, 5, 5]))
+
+    def test_score_bonus_arithmetic(self):
+        bonus = Bonus(100, [1, 2, 3])
+        leaf = Leaf(0, [1, 2, 3, 4, 5])
+        diff = leaf - bonus
+        self.assertEqual(100, diff.score)
+        self.assertEqual([4, 5], diff.rolls)
